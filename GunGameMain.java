3cef45cfcf41282314f86976f5169c3e512c2ccc
PixelorsStyx <varulnima@gmail.com>
2016-03-31 17:47:37 +0200

    Update GunGameMain.java

--- GunGameMain.java
+++ GunGameMain.java
@@ -4,15 +4,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class GunGameMain extends JavaPlugin implements CommandExecutor {
+public class GunGameMain extends JavaPlugin {
 
-	public String prefix = "§6[§bGunGame§6] ";
+	public String prefix = "Â§6[Â§bGunGameÂ§6] ";
 
 	public ArenaManager manager;
 
@@ -27,7 +26,7 @@ public class GunGameMain extends JavaPlugin implements CommandExecutor {
 		FileConfiguration cfg = this.getConfig();
 		cfg.options().copyDefaults(true);
 		this.saveConfig();
-
+		getLogger().info("[GunGame] Was successfully loaded!");
 		if (this.getConfig().getString("mainSpawn") != null) {
 			this.mainSpawn = stringToLoc(this.getConfig().getString("mainSpawn"));
 		}
@@ -51,21 +50,22 @@ public class GunGameMain extends JavaPlugin implements CommandExecutor {
 			Player p = (Player) sender;
 
 			if (args.length == 0) {
-				sender.sendMessage("§6 §m------------ §bGunGame §6§m------------");
-				sender.sendMessage("§aVersion: §b" + this.getDescription().getVersion());
-				sender.sendMessage("§aAuthor: §b" + this.getDescription().getAuthors());
-				sender.sendMessage("§6 §m------------ §bGunGame §6§m------------");
+				sender.sendMessage("Â§6 Â§m------------ Â§bGunGame Â§6Â§m------------");
+				sender.sendMessage("Â§aVersion: Â§b" + this.getDescription().getVersion());
+				sender.sendMessage("Â§aAuthor: Â§b" + this.getDescription().getAuthors());
+				sender.sendMessage("Â§6 Â§m------------ Â§bGunGame Â§6Â§m------------");
+				return true;
 			}
-			
+
 			if (args[0].equalsIgnoreCase("join")) {
 
 				if (args.length == 1) {
-					p.sendMessage(prefix + "§c/gg join <ArenaID>");
+					p.sendMessage(prefix + "Â§c/gg join <ArenaID>");
 				} else {
 					int arenaID = Integer.valueOf(args[1]);
 					this.manager.players.put(p.getName(), arenaID);
-					
-					p.sendMessage(prefix + "§aSuccessfully joined the game §eGG-" + args[1] + "!");
+
+					p.sendMessage(prefix + "Â§aSuccessfully joined the game Â§eGG-" + args[1] + "!");
 				}
 
 			}
@@ -75,74 +75,76 @@ public class GunGameMain extends JavaPlugin implements CommandExecutor {
 				Player p = (Player) sender;
 
 				if (!p.hasPermission("gungame.admin")) {
-					p.sendMessage(prefix + "§cYou don't have the Permission to use that Command!");
+					p.sendMessage(prefix + "Â§cYou don't have the Permission to use that Command!");
 					return true;
-				}
+				} else {
 
-				if (args.length == 0) {
-					p.sendMessage("§cGunGame-Help:");
-					p.sendMessage("§6/gga setlobby §8to set the arenalobby");
-					p.sendMessage("§6/gga addspawn §8to add an arenaspawn");
-					p.sendMessage("§6/gga resetspawns §8to reset the arenaspawns");
-					p.sendMessage("§6/gga addarena §8to create an arena!");
+					if (args.length == 0) {
+						p.sendMessage("Â§cGunGame-Help:");
+						p.sendMessage("Â§6/gga setlobby <ID>Â§8to set the arenalobby");
+						p.sendMessage("Â§6/gga addspawn <ID> Â§8to add an arenaspawn");
+						p.sendMessage("Â§6/gga resetspawns <ID> Â§8to reset the arenaspawns");
+						p.sendMessage("Â§6/gga addarena <ID> Â§8to create an arena!");
+						return true;
 
-				}
-
-				if (args[0].equalsIgnoreCase("setmainspawn")) {
-
-					this.mainSpawn = p.getLocation();
-					this.getConfig().set("mainSpawn", locToString(this.mainSpawn));
-					p.sendMessage(prefix + "§aThe main spawn was successfully set!");
-
-				}
-				if (args[0].equalsIgnoreCase("addarena")) {
-
-					this.manager.createArena(p);
-					p.sendMessage(prefix + "§aSuccessfully created a new Arena!");
-
-				}
-				if (args[0].equalsIgnoreCase("setlobby")) {
-
-					if (args.length == 1) {
-						p.sendMessage(prefix + "§c/gga setlobby <ArenaID>");
-					} else {
-						int arenaID = Integer.valueOf(args[1]);
-						Arena a = this.manager.getArena(arenaID);
-						a.setLobby(p.getLocation());
-						p.sendMessage(prefix + "§aSuccessfully updated LobbyLocation!");
 					}
 
-				}
-				if (args[0].equalsIgnoreCase("addspawn")) {
+					if (args[0].equalsIgnoreCase("setmainspawn")) {
 
-					if (args.length == 1) {
-						p.sendMessage(prefix + "§c/gga addspawn <ArenaID>");
-					} else {
-						int arenaID = Integer.valueOf(args[1]);
-						Arena a = this.manager.getArena(arenaID);
-						a.addSpawn(p.getLocation());
-						p.sendMessage(prefix + "§aSuccessfully added SpawnLocation!");
+						this.mainSpawn = p.getLocation();
+						this.getConfig().set("mainSpawn", locToString(this.mainSpawn));
+						p.sendMessage(prefix + "Â§aThe main spawn for GunGame was successfully set!");
+
+					}
+					if (args[0].equalsIgnoreCase("addarena")) {
+
+						this.manager.createArena(p);
+						p.sendMessage(prefix + "Â§aSuccessfully created a new Arena!");
+
+					}
+					if (args[0].equalsIgnoreCase("setlobby")) {
+
+						if (args.length == 1) {
+							p.sendMessage(prefix + "Â§c/gga setlobby <ArenaID>");
+						} else {
+							int arenaID = Integer.valueOf(args[1]);
+							Arena a = this.manager.getArena(arenaID);
+							a.setLobby(p.getLocation());
+							p.sendMessage(prefix + "Â§aSuccessfully updated the LobbyLocation of Â§6" + arenaID + "!");
+						}
+
+					}
+					if (args[0].equalsIgnoreCase("addspawn")) {
+
+						if (args.length == 1) {
+							p.sendMessage(prefix + "Â§c/gga addspawn <ArenaID>");
+						} else {
+							int arenaID = Integer.valueOf(args[1]);
+							Arena a = this.manager.getArena(arenaID);
+							a.addSpawn(p.getLocation());
+							p.sendMessage(prefix + "Â§aSuccessfully added a SpawnLocation of Â§6" + arenaID + "!");
+						}
+
 					}
 
-				}
+					if (args[0].equalsIgnoreCase("resetspawns")) {
 
-				if (args[0].equalsIgnoreCase("resetspawns")) {
-
-					if (args.length == 1) {
-						p.sendMessage(prefix + "§c/gga resetspawns <ArenaID>");
-					} else {
-						int arenaID = Integer.valueOf(args[1]);
-						Arena a = this.manager.getArena(arenaID);
-						a.resetSpawns();
-						p.sendMessage(prefix + "§aSuccessfully reset SpawnLocations!");
+						if (args.length == 1) {
+							p.sendMessage(prefix + "Â§c/gga resetspawns <ArenaID>");
+						} else {
+							int arenaID = Integer.valueOf(args[1]);
+							Arena a = this.manager.getArena(arenaID);
+							a.resetSpawns();
+							p.sendMessage(prefix + "Â§aSuccessfully reset SpawnLocations of Â§6" + arenaID + "!");
+						}
 					}
 				}
 
 			} else {
-				sender.sendMessage(prefix + "§cOnly for Players!");
+				sender.sendMessage(prefix + "Â§cOnly for Players!");
 			}
 		} else {
-			sender.sendMessage(prefix + "§cTry /gg");
+			sender.sendMessage(prefix + "Â§cTry /gg");
 		}
 
 		return true;
